ba721c13cff0d194b4c11655aef936b67e998bd9
maha9112 <132583447+maha9112@users.noreply.github.com>
2023-11-15 17:09:22 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "AiChat",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "AiChat",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
